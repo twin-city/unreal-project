8214e42ad296c66518580815e291bbc789fba12e
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-02-22 11:47:46 +0100

    [paramsMenuWidget] Set light position according to hour input

---
+++ Source/TwinCity/Clock.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Clock.h"
+
+// Sets default values
+AClock::AClock()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AClock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	BeginTime = FPlatformTime::Seconds();
+	
+}
+
+// Called every frame
+void AClock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CurrentTime = FTimespan(FTimespan::FromHours(FPlatformTime::Seconds() - BeginTime));
+
+	CurrentDays = CurrentTime.GetDays();
+	CurrentMinutes = CurrentTime.GetMinutes();
+	
+	if (TmpHours == -1.f)
+	{
+		CurrentHours = CurrentTime.GetHours();
+	}
+	else
+	{
+		CurrentHours = TmpHours;
+		TmpHours = -1.f;
+		BeginTime = FPlatformTime::Seconds() - CurrentHours;
+	}
+	CurrentTime = FTimespan(CurrentDays, CurrentHours, CurrentMinutes, 0);
+
+}
+
+// Called to bind functionality to input
+void AClock::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
