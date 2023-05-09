1d672811feb1b48cc55e6da4fac104c78959c90c
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-05-09 15:35:33 +0200

    Update repo

--- Source/TwinCity/TwinCity.Build.cs
+++ Source/TwinCity/TwinCity.Build.cs
@@ -8,6 +8,6 @@ public class TwinCity : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "ZoneGraph" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Json", "JsonUtilities" });
 	}
 }
