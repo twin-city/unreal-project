72194c9ac55283ecac11b4f30b175970fd75d792
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-05-09 15:34:47 +0200

    Update repo

---
+++ Source/TwinCity/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MyBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+
+class FJsonObject;
+
+USTRUCT(BlueprintType, Category="MetaDatasStruct")
+struct FMetaDatasStruct
+{
+	GENERATED_USTRUCT_BODY()
+
+	public:
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		FString	District = "";
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		int	Hour = 0;
+		
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		int	Day = 0;
+		
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		int	Month = 0;
+		
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		int	Year = 0;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		FString	Weather = "";
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		FRotator	CameraRotation = { 0.f, 0.f, 0.f };
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		FVector	CameraLocation = { 0.f, 0.f, 0.f };
+		
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		int	VehiclesNb = 0;
+		
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		TMap<int, FString> Peds;
+
+		// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Data")
+		// FDateTime Date;
+
+};
+
+UCLASS()
+class TWINCITY_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	public:
+
+		UFUNCTION(BlueprintCallable, Category="MetaDatas")
+		static void	WriteStructToJsonFile(FString const FilePath, FMetaDatasStruct Struct);
+	
+		static void	WriteMetaDatasToFile(FString const FilePath, TSharedPtr<FJsonObject> JsonObject);
+	
+};
